e4cdf1fd949589891432e21abdf923ffd2a65960
kayllayne <kayllaynemaria210@gmail.com>
2025-05-05 13:25:48 -0300

    novo

--- parte1.php
+++ parte1.php
@@ -3,6 +3,8 @@ $nome = $_POST['nome'];
 $sobrenome = $_POST['sobrenome'];
 $nascimento = $_POST['nascimento'];
 $email = $_POST['email'];
+$telefone = $_POST['telefone']
+$CPF = $_POST['cpf']
 
 echo "<h1>Cadastro Realizado</h1>";
 echo "O participante <strong>$nome</strong> foi cadastrado com sucesso.";
