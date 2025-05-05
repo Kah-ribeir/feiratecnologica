e994e45d9901bd0963ce790ad274b6181be92b10
kayllayne <kayllaynemaria210@gmail.com>
2025-05-05 10:03:45 -0300

    feira de tecnologia

---
+++ parte1.php
@@ -0,0 +1,9 @@
+<?php
+$nome = $_POST['nome'];
+$sobrenome = $_POST['sobrenome'];
+$nascimento = $_POST['nascimento'];
+$email = $_POST['email'];
+
+echo "<h1>Cadastro Realizado</h1>";
+echo "O participante <strong>$nome</strong> foi cadastrado com sucesso.";
+?>
---
+++ parte2.php
@@ -0,0 +1,8 @@
+<?php
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$assunto = $_POST['assunto'];
+
+echo "<h1>Mensagem Enviada</h1>";
+echo "O e-mail de <strong>$nome</strong> foi enviado com sucesso.";
+?>
